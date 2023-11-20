4d77c9d1a8b7de627f960742e96543dc2b4ad64b
daniil <danilka.radaev.00@mail.ru>
2023-11-20 11:34:52 +0300

    weather_parser.py

--- TestingParse/test_parsing/Json_test/weather_parser.py
+++ TestingParse/test_parsing/Json_test/weather_parser.py
@@ -3,5 +3,9 @@ import requests
 response = requests.get('https://parsinger.ru/3.4/1/json_weather.json')
 
 list = list(response.json())
-
-lambda x: filter(x['Температура воздуха'] - 1, list)
+date = list[0]['Дата']
+temreture = list[0]['Температура воздуха']
+for element in response:
+    for key, val in element.items():
+        if (temreture > val):
+            print("sa")
