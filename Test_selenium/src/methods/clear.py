e22744bea02795ee3a3739d68c4810f2e6c634a4
daniil <danilka.radaev.00@mail.ru>
2023-12-21 03:35:42 +0300

    clear

---
+++ Test_selenium/src/methods/clear.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+with webdriver.Chrome() as chrome:
+    chrome.get('https://parsinger.ru/selenium/5.5/1/1.html')
+
+    elements = chrome.find_elements(By.CLASS_NAME, 'text-field')
+
+    for element in elements:
+        element.clear()
+    chrome.find_element(By.ID, 'checkButton').click()
+    time.sleep(5)
